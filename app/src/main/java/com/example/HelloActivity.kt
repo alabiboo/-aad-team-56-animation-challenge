c4bbe6442cdd0604f5b93475d30ce6eaf7813e95
Severin Lawson <severinlawson@gmail.com>
2019-10-23 07:49:35 +0000

    Animation2

--- app/src/main/java/com/example/HelloActivity.kt
+++ app/src/main/java/com/example/HelloActivity.kt
@@ -1,24 +1,80 @@
 package com.example
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.BounceInterpolator
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HelloActivity : AppCompatActivity() {
 
-
+    private var desc: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello)
-        val imageView : ImageView= findViewById<ImageView>(R.id.helloImg)
-        val btnAnime : Button = findViewById<Button>(R.id.btnAnime)
+        desc = findViewById<TextView>(R.id.desText)
+        val btnAnime : Button = findViewById<Button>(R.id.btnAnime2)
 
-        btnAnime.setOnClickListener { imageView.animate()
-            .translationXBy(-1000f)
-            .duration = 2000 }
+        btnAnime.setOnClickListener {
+            if (desc != null) {
+                desc!!.visibility = View.VISIBLE
+                combin_anims()
+            }
+        }
 
+
+    }
+
+    private fun combin_anims() {
+
+        val anims1 = AnimatorSet()
+        val sX = ObjectAnimator.ofFloat(desc, View.SCALE_X, 0.2f, 1.0f)
+        val sY = ObjectAnimator.ofFloat(desc, View.SCALE_Y, 0.2f, 1.0f)
+        val fade = ObjectAnimator.ofFloat(desc, View.ALPHA, 0.2f, 1.0f)
+
+        anims1.playTogether(sX, sY, fade)
+        anims1.setDuration(600)
+
+        val anims2 = AnimatorSet()
+
+        val tx1 = ObjectAnimator.ofFloat(desc, View.TRANSLATION_Y, 0f, 200f)
+        tx1.setDuration(1000)
+        tx1.interpolator = BounceInterpolator()
+
+        val rotate = ObjectAnimator.ofFloat(desc, View.ROTATION, -360f, 0f)
+        rotate.setDuration(1000)
+        rotate.interpolator = AccelerateInterpolator()
+
+        anims2.playTogether(tx1, rotate)
+
+        val final_anim = AnimatorSet();
+
+        final_anim.play(anims1).before(anims2)
+        final_anim.play(anims2)
+
+        final_anim.addListener(object: Animator.AnimatorListener{
+            override fun onAnimationRepeat(p0: Animator?) {
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+            }
+
+            override fun onAnimationStart(p0: Animator?) {
+            }
+
+        });
+
+        final_anim.start()
 
     }
 }
